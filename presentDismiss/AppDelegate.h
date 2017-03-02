17d6841515c913b92a137785bb940f197414e811
praveen <praveen@gmail.com>
2017-03-02 15:46:02 -0500

    Initial Commit

---
+++ presentDismiss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  presentDismiss
+//
+//  Created by Praveen on 2017-03-02.
+//  Copyright © 2017 Praveen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
